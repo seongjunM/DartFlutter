55209dc2083a8cf659e8622dde0d96c0529ba120
Sungjun <thesky_09@naver.com>
2025-06-30 23:35:26 +0900

    BasicSyntax.dart updated

--- p1/BasicSyntax.dart
+++ p1/BasicSyntax.dart
@@ -1,1 +1,13 @@
+class Point {
+  var x;
+  var y;
+
+  Point([var numX = 0, var numY = 0]) {
+    this.x = numX;
+    this.y = numY;
+  }
+
+  String get stringfy => "(${this.x}, ${this.y})";
+}
+
 void main() {}
